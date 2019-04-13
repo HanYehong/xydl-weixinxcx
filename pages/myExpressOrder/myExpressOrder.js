b2ccd2c18e00a47b5e5cae62f156d662eecd1dbc
yehongHan <yehong.han@ly.com>
2019-04-13 17:04:54 +0800

    我的订单---进行中---初步布局完成

--- pages/myExpressOrder/myExpressOrder.js
+++ pages/myExpressOrder/myExpressOrder.js
@@ -1,4 +1,5 @@
 var app = getApp();
+let config = require("../../config/config");
 Page({
   /**
    * 页面的初始数据
@@ -7,31 +8,85 @@ Page({
     currentIndex: 0,
     orderListDoing: [
       {
-        expressType:'',
-        getCode:'',
-        destination:'',
-        money:''
+        expressType: 0,
+        getCode:'8526',
+        destination:'东七C205',
+        money:'2',
+        color:'',
+        name: '',
+        status: '',
+        statusText: ''
       },
       {
-        expressType:'',
-        getCode:'',
-        destination:'',
-        money:''
+        expressType: 1,
+        getCode:'1235',
+        destination:'东八楼下',
+        money:'4',
+        color:'',
+        name: '',
+        status: '',
+        statusText: ''
       },
       {
-        expressType:'',
-        getCode:'',
-        destination:'',
-        money:''
+        expressType: 2,
+        getCode:'364',
+        destination:'北大活',
+        money:'2',
+        color:'',
+        name: '',
+        status: '',
+        statusText: ''
       },
       {
-        expressType:'',
-        getCode:'',
-        destination:'',
-        money:''
+        expressType: 3,
+        getCode:'2365',
+        destination:'南管B206',
+        money:'3',
+        color:'',
+        name: '',
+        status: '',
+        statusText: ''
       }
     ],
   },
+
+  onLoad: function (options) {
+    console.log(this.data.orderListDoing);
+    let orderListDoingAnother = this.data.orderListDoing;
+    for (let i = 0; i < orderListDoingAnother.length; i++) {
+      orderListDoingAnother[i].color = this.getColorById(orderListDoingAnother[i].expressType);
+    }
+    for (let i = 0; i < orderListDoingAnother.length; i++) {
+      orderListDoingAnother[i].name = this.getNameById(orderListDoingAnother[i].expressType).substring(0, 2);
+    }
+    this.setData({
+      orderListDoing: orderListDoingAnother
+    })
+    console.log(this.data.orderListDoing);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  getColorById(id) {
+    for (let i = 0; i < config.EXPRESS.length; i++) {
+      if (config.EXPRESS[i].ID == id) {
+        return config.EXPRESS[i].COLOR;
+      }
+    }
+  },
+  getNameById(id) {
+    for (let i = 0; i < config.EXPRESS.length; i++) {
+      if (config.EXPRESS[i].ID == id) {
+        return config.EXPRESS[i].NAME;
+      }
+    }
+  },
+
   //swiper切换时会调用
   pagechange: function (e) {
     if ("touch" === e.detail.source) {
